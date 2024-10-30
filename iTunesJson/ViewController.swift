25b60f21111aea85f74856cf813861c51f9974e2
Greg Bolsinga <bolsinga@mac.com>
2024-10-30 11:24:51 -0700

    Initial Commit

---
+++ iTunesJson/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iTunesJson
+//
+//  Created by Greg Bolsinga on 10/30/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
