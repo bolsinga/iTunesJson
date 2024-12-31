7176239e88e22618d6e9e6fecb3704a0dadf24a0
Greg Bolsinga <bolsinga@mac.com>
2024-12-31 08:36:15 -0800

    Simplify calling into external program. (#15)
    Following [this suggestion](https://github.com/apple/swift-argument-parser/issues/688#issuecomment-2565080206), thanks!

--- iTunesJson/Program.swift
+++ iTunesJson/Program.swift
@@ -5,19 +5,12 @@
 //  Created by Greg Bolsinga on 10/30/24.
 //
 
-import ArgumentParser
 import Foundation
 import iTunes
 
 @main
-public struct Program: AsyncParsableCommand {
-  public static let configuration = CommandConfiguration(
-    commandName: "tunes",
-    abstract: "A tool for working with iTunes data.",
-    version: iTunesVersion,
-    subcommands: [BackupCommand.self, PatchCommand.self, RepairCommand.self, BatchCommand.self],
-    defaultSubcommand: BackupCommand.self
-  )
-
-  public init() {}  // This is public and empty to help the compiler.
+public struct Program {
+  static func main() async {
+    await iTunes.Program.main()
+  }
 }
